336fe423003986d0e904a8ab4a8cbf44f61b78a7
Matex2395 <mateorgafjo@gmail.com>
2025-01-28 11:46:39 -0500

    Implementar Factory en Registro Se ha reutilizado el método de crear usuario de Factory para el registro de usuarios.

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -6,16 +6,19 @@ using System.Security.Claims;
 using TaskTimePredicter.Data;
 using TaskTimePredicter.Models;
 using TaskTimePredicter.ViewModels;
+using TaskTimeDesignPatterns.Interfaces;
 
 namespace TaskTimePredicter.Controllers
 {
     public class AccessController : Controller
     {
         private readonly AppDbContext _db;
+        private readonly IUserFactory _userFactory;
 
-        public AccessController(AppDbContext context)
+        public AccessController(AppDbContext context, IUserFactory userFactory)
         {
             _db = context;
+            _userFactory = userFactory;
         }
 
         [HttpGet]
@@ -44,21 +47,33 @@ namespace TaskTimePredicter.Controllers
                 return View();
             }
 
-            User user = new User
+            try
             {
-                UserName = model.Name,
-                UserEmail = model.Email,
-                UserPassword = model.Password,
-                CreatedAt = DateOnly.FromDateTime(DateTime.Now),
-                UserRole = "Developer"
-            };
+                // Usar UserFactory para crear un nuevo usuario
+                User user = _userFactory.CreateUser(
+                    userName: model.Name,
+                    userEmail: model.Email,
+                    userPassword: model.Password,
+                    userRole: "Developer" // Asignar rol de "Desarrollador" por defecto
+                );
 
-            await _db.Users.AddAsync(user);
-            await _db.SaveChangesAsync();
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
 
-            if (user.UserId != 0) return RedirectToAction("Login", "Access");
+                if (user.UserId != 0)
+                {
+                    return RedirectToAction("Login", "Access");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = "Ha ocurrido un error inesperado al registrar el usuario.";
+            }
 
-            TempData["ErrorMessage"] = "Ha ocurrido un error al registrar el usuario.";
             return View();
         }
 
